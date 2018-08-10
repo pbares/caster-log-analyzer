1e363095e051ac6d25f5f2e3582f3f4f17ab1af7
Paul Bares <pba@activeviam.com>
2018-08-10 15:55:18 +0200

    Remove filters button

--- src/App.js
+++ src/App.js
@@ -22,9 +22,16 @@ class FilterCheckBox extends Component {
   }
 
   render() {
+    const inputProps = {
+      onChange: this.handleChange,
+      id: this.props.value,
+      value: this.props.value,
+      checked: this.props.checked,
+    };
+
     return (
       <div className="FilterCheckBoxContainer">
-        <input type="checkbox" id={this.props.value} value={this.props.value} onChange={this.handleChange}/>
+        <input type="checkbox" {...inputProps}/>
         <label htmlFor={this.props.value}>
           <font color={this.props.color}>{this.props.value}</font>
         </label>
@@ -34,7 +41,7 @@ class FilterCheckBox extends Component {
 }
 
 /**
- * Container to store the analyzed result 
+ * Container that stores the analyzed result 
  */
 class ResultArea extends Component {
   render() {
@@ -50,7 +57,7 @@ class InputArea extends Component {
     this.state = {
       text: "",
     };
-
+    
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -60,29 +67,42 @@ class InputArea extends Component {
   }
 
   clear() {
-    this.setState({text: ""});
+    this.setState({text: ''});
   }
 
   triggerAnalysis() {
     this.props.onUpdate(this.state.text);
   }
+
+  toggleRemoveFilter() {
+    this.props.onUpdatePredicateList(null);
+  }
   
   render() {
-    const filterCheckBoxes = FILTERS.map(f => 
-       <FilterCheckBox onUpdatePredicateList={this.props.onUpdatePredicateList} 
-            value={f.value} pattern={f.pattern} color={f.color}/>
-    );
+    const filterCheckBoxes = FILTERS.map(f => {
+        const inputProps = {
+          onUpdatePredicateList: this.props.onUpdatePredicateList,
+          key: f.value,
+          value: f.value,
+          pattern : f.pattern,
+          color: f.color,
+          checked: this.props.selectedItems.has(f.value),
+        };
 
+        return <FilterCheckBox {...inputProps}/>
+    });
+    
     return (
       <div>
         <textarea 
           className="textBox" 
-          placeholder="log to analyze"
+          placeholder="copy/paste here the log to analyze"
           value={this.state.text}
           onChange={this.handleChange}
         />
 
         <div>
+          <button className="Button" onClick={() => this.toggleRemoveFilter()}>Remove filters</button>
           <button className="Button" onClick={() => this.triggerAnalysis()}>Analyze</button>
           <button className="Button" onClick={() => this.clear()}>Clear</button>
 
@@ -108,21 +128,30 @@ class App extends Component {
     this.setState({value: val});
   }
 
+  /**
+   * Updates the list of filters by appending the given predicate to the existing list. 
+   * If null is passed as argument, it means 'no filter'.
+   * 
+   * @param {*} item 
+   */
   updatePredicateList(item) {
-    var found = false;
-    const predicates = this.state.predicates.slice();
-    for(var i = 0; i < predicates.length; i++) {
-      if(predicates[i].id === item.id) {
-        // Remove it
-        found = true;
-        predicates.splice(i, 1);
-        break;
+    let predicates = [];
+    if(item !== null){
+      var found = false;
+      predicates = this.state.predicates.slice();
+      for(var i = 0; i < predicates.length; i++) {
+        if(predicates[i].id === item.id) {
+          // Remove it
+          found = true;
+          predicates.splice(i, 1);
+          break;
+        }
       }
-    }
-
-    if(!found) {
-      // Add it to the list
-      predicates.push(item);
+  
+      if(!found) {
+        // Add it to the list
+        predicates.push(item);
+      }
     }
     this.setState({predicates: predicates});
   }
@@ -130,12 +159,15 @@ class App extends Component {
   render() {
     const result = applyPredicatesOnText(this.state.value, this.state.predicates);
 
+    const selectedItems = new Set();
+    this.state.predicates.map(p => selectedItems.add(p.id));
+
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <h1 className="App-title">Caster log analyzer</h1>
-        </header> */}
-        <InputArea onUpdate={this.update} onUpdatePredicateList={this.updatePredicateList}/>
+        <InputArea 
+          onUpdate={this.update} 
+          onUpdatePredicateList={this.updatePredicateList}
+          selectedItems={selectedItems}/>
         <ResultArea value={result}/>
       </div>
     );
